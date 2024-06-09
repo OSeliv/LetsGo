37925f817301f46158fb29d06f7eced65d04552d
Olka <selo@automacon.ru>
2024-06-09 21:25:12 +0300

    less16.2

--- lesson16/task2/main.go
+++ lesson16/task2/main.go
@@ -4,31 +4,33 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"time"
 )
 
 func main() {
 	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
 	defer cancel()
 	wg := sync.WaitGroup{}
-
+	wg.Add(5)
 	for i := 0; i < 5; i++ {
 		i := i
-		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			for {
 				select {
 				case <-ctx.Done():
-					fmt.Println("отмена горутины: ", i)
+					fmt.Println("отмена горутины: ", i, ctx.Err())
 					return
 				default:
 					fmt.Println("работа горутины: ", i)
+					time.Sleep(2 * time.Second)
 				}
 			}
 		}()
 	}
 	wg.Add(1)
+
 	go func() {
 		defer wg.Done()
 		fmt.Println("стоп!")
